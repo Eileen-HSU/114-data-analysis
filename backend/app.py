b1a873f4050a0ef5a00cd48aa7bc761c57bdbb78
Eileen Hsu <bingq7943@gmail.com>
2026-05-15 18:37:23 +0800

    app.py修改

--- backend/app.py
+++ backend/app.py
@@ -60,7 +60,6 @@ start_scheduler()
 @app.before_request
 def handle_options():
     if request.method == "OPTIONS":
-        from flask import make_response
         response = make_response()
         response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
