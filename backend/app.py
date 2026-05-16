b0e10025381a4ef44e093470168ead763df9b963
Eileen Hsu <bingq7943@gmail.com>
2026-05-16 17:34:41 +0800

    app.py修改

--- backend/app.py
+++ backend/app.py
@@ -62,15 +62,6 @@ app.register_blueprint(workspace_bp)
 
 start_scheduler()
 
-@app.before_request
-def handle_options():
-    if request.method == "OPTIONS":
-        response = make_response()
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        return response, 200
-
 @app.route("/api/status", methods=["GET"])
 def get_status():
     return jsonify({
